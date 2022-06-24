5ecce74d2c23550667e3012b1ff2c1f4b0956780
Mahmoud AlBoraey <mahmoudalsayied@gmail.com>
2022-06-24 22:56:11 +0200

    done creating the basic struchuer of redux

---
+++ src/Redux/types.js
@@ -0,0 +1,4 @@
+export const CREATE_NEW_TODO = 'CREATE_NEW_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const CHECK_CURRENT_TODO = 'CHECK_CURRENT_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
